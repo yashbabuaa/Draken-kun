3ae686e286e1911a30d6c54992dfa0579fbd42dd
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-06-23 22:40:56 +0530

    update __main__.py

--- Draken/__main__.py
+++ Draken/__main__.py
@@ -3,7 +3,7 @@ import os
 from telethon import *
 
 bot_token = os.environ.get('BOT_TOKEN')
-api_id = os.environ.get(int('API_ID'))
+api_id = os.environ.get('API_ID')
 api_hash = os.environ.get('API_HASH')
 
 draken = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
