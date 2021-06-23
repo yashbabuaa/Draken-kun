a8cc084d1efcdc4d56f34a7f375f443e7bbf83da
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-06-23 23:19:02 +0530

    update __main__.py

--- Draken/__main__.py
+++ Draken/__main__.py
@@ -2,17 +2,17 @@ import logging
 import os 
 from telethon import TelegramClient, events, Button
 
-bot_token = os.environ.get('BOT_TOKEN')
+draken_token = os.environ.get('draken_TOKEN')
 api_id = os.environ.get('API_ID')
 api_hash = os.environ.get('API_HASH')
 
-bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
+draken = TelegramClient('bot', api_id, api_hash).start(draken_token=draken_token)
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level = logging.INFO)
 
 logger = logging.getLogger("__name__")
 
-@bot.on(events.NewMessage(incoming=True, pattern=r'^#request(.*)'))
+@draken.on(events.NewMessage(incoming=True, pattern=r'^#request(.*)'))
 async def request(mikey):
   query = mikey.message.text.split(" ", 1)
   chat = -1001487075546
@@ -22,7 +22,7 @@ async def request(mikey):
     await mikey.message.reply("Request something bakayaro!")
     return
   keybo = []
-  async for message in bot.iter_message(chat):
+  async for message in draken.iter_message(chat):
     title = message.text[:20]
     msg_id = message.id 
     link = f"https://t.me/c/{str(chat)[4:]}/{str(msg_id)}" 
@@ -31,20 +31,23 @@ async def request(mikey):
     req_user = f"[{mikey.from_user.first_name}](tg://user?id={mikey.from_user.id})" 
     message_link = f"https://t.me/c/1364238597/{mikey.message.id}"
     text = f"Request: {query}\nRequested by: {req_user}\nMessage link: [limk]({link})"
-    await bot.send_message(-1001226512514, text)
+    await draken.send_message(-1001226512514, text)
     await mikey.message.reply_text("Roger! Request sent, Now wait like a good citizen.")
     return
-  m = await mikey.message.reply("Found Some Results!", buttons = [[Button.url(text = "Check Pm!", url = "http://t.me/botROBOT")]])
+  m = await mikey.message.reply("Found Some Results!", buttons = [[Button.url(text = "Check Pm!", url = "http://t.me/drakenROdraken")]])
   try:
-    await bot.send_message(message.from_user.id, text = "Found some matches for you!", buttons = [keybo])
+    await draken.send_message(message.from_user.id, text = "Found some matches for you!", buttons = [keybo])
   except UserIsBlockedError:
-    await m.edit("I haven't met you yet please start me and request again!", button = [[Button.url(text="Start", url = "https://t.me/botROBOT")]])
+    await m.edit("I haven't met you yet please start me and request again!", button = [[Button.url(text="Start", url = "https://t.me/drakenROdraken")]])
   
     
-@bot.on(events.NewMessage(incoming=True, pattern=r'^/start|/start@botROBOT')) 
+@draken.on(events.NewMessage(incoming=True, pattern=r'^/start|/start@drakenROdraken')) 
 async def start(mikey):
-  await mikey.message.reply('Ahh you can request now @SeriesArchiveDiscussion, Im a bot specifically made just to handle request, so go and request if you want something!!!', buttons = [[Button.url(text = "SeriesArchiveDiscussion", url = "https://t.me/SeriesArchiveDiscussion")], [Button.url(text = "Creator", url = "https://t.me/DontKnowWhoRU")]])
+  await mikey.message.reply('Ahh you can request now @SeriesArchiveDiscussion, Im a draken specifically made just to handle request, so go and request if you want something!!!', buttons = [[Button.url(text = "SeriesArchiveDiscussion", url = "https://t.me/SeriesArchiveDiscussion")], [Button.url(text = "Creator", url = "https://t.me/DontKnowWhoRU")]])
 
 
 print('Starting.......')
 print('Im online!!!')
+
+draken.start()
+draken.run_until_disconnected()
