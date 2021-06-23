4724832d457fbb6527d25f0cff4ce238abaeabb6
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-06-23 21:49:53 +0530

    update __main__.py

--- Draken/__main__.py
+++ Draken/__main__.py
@@ -5,5 +5,5 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger("__name__")
 
-print('Starting.......)
+print('Starting.......')
 print('Im online!!!')
