c399426c771c4ec5e512bf0482e911404f843272
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-06-23 21:25:08 +0530

    Add files via upload

---
+++ Draken/__main__.py
@@ -0,0 +1,9 @@
+from Draken import draken
+import logging 
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level = logging.INFO)
+
+logger = logging.getLogger("__name__")
+
+print('Starting.......)
+print('Im online!!!')
