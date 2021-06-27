768f0df7024b6acd0a3e7b1244d1d50cf1929945
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-06-27 10:43:16 +0530

    update __main__.py

--- Draken/__main__.py
+++ Draken/__main__.py
@@ -4,6 +4,8 @@ from telethon import TelegramClient, events, Button
 from telethon.sessions import StringSession
 from telethon import errors
 from telethon.tl.types import InputMessagesFilterDocument
+from telegram import * 
+from telegram.ext import *
 print("Starting....")
 
 draken_token = os.environ.get('BOT_TOKEN')
@@ -13,8 +15,8 @@ string = os.environ.get('STRING_SESSION')
 bot_name = os.environ.get('BOT_NAME', 'Draken')
 
 
-draken = TelegramClient('bot', api_id, api_hash).start(bot_token=draken_token)
-
+draken = Updater(draken_token)
+dispatcher = draken.dispatcher
 takemichi = TelegramClient(StringSession(string), api_id, api_hash)
 
 if takemichi:
@@ -24,27 +26,34 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger("__name__")
 
-@draken.on(events.NewMessage(incoming=True, pattern=r'^\!search(.*)'))
-@draken.on(events.NewMessage(incoming=True, pattern=r'^#request(.*)'))
-async def request(mikey):
+def request(update: Update, context: CallbackContext):
   chat = -1001487075546
   chat2 = -1001550963689
-  if mikey.is_private:
-    return 
-  query = mikey.message.text.split(" ", 1)
+  bot = context.bot
+  chat = upsate.effective_chat 
+  user = update.effective_user 
+  message = update.effective_message
+  if chat.type == "private":
+    if message.text.startswith("!search"):
+      pass
+    else:
+      return 
+  query = message.text.split(" ", 1)
   try:
     query = query[1]
   except IndexError:
     await mikey.reply("Request something bakayaro!")
     return
-  if not mikey.chat_id == -1001364238597:
+  if not chat.id == -1001364238597:
     req_log = "False"
-  elif mikey.message.text.startswith("!search"):
+  elif message.text.startswith("!search"):
     req_log = "False"
   else:
     req_log = "True"
-  if mikey.reply_to_msg_id:
-    mikey = await mikey.get_reply_message()
+  if message.reply_to_message:
+    mikey = message.reply_to_message
+  else:
+    mikey = message
   keybo = []
   count = 0
   text = ''
@@ -53,44 +62,45 @@ async def request(mikey):
       text = f"{message.text[2:20]}..."
       msg_id = message.id 
       link = f"https://t.me/c/{str(chat)[4:]}/{str(msg_id)}" 
-      keybo.append([Button.url(text = text, url = link)])
+      keybo.append([InlineKeyboardButton(text=f"{text}", url = link)])
     except TypeError:
       pass
   count2 = 0
   if keybo == []:
     async for message in takemichi.iter_messages(chat2, search = query, reverse = True, filter = InputMessagesFilterDocument):
-      await takemichi.send_message(mikey.chat_id, file = message.document)
-      count2 += 1 
+      bot.send_document(chat.id, document = message.document)
     if not count2 == 0:
-      await mikey.reply("👆")
+      await mikey.reply_text("👆")
     if count2 == 0:
       if req_log == "True":
-        req_user = f"[{mikey.sender.first_name}](tg://user?id={mikey.sender_id})" 
-        message_link = f"https://t.me/c/1364238597/{mikey.id}"
+        req_user = f"[{mikey.from_user.first_name}](tg://user?id={mikey.from_user.id})" 
+        message_link = f"https://t.me/c/1364238597/{mikey.message_id}"
         text = f"Request: {query}\nRequested by: {req_user}\n"
-        await draken.send_message(-1001226512514, text, buttons = [[Button.url(text = "Message", url = message_link)], [Button.inline(text="Request Complete", data = "recomp")]])
-        await mikey.reply("Roger! Request sent, Now wait like a good citizen.")
+        bot.send_message(-1001226512514, text, buttons = [[InlineKeyboardButton(text = "Message", url = message_link)], [InlineKeyboardButton(text="Request Complete", callback_data = "recomp")]])
+        mikey.reply("Roger! Request sent, Now wait like a good citizen.")
         return
       else:
-        await mikey.reply("Gotcha, Now wait like a good citizen!!")
+        mikey.reply("Gotcha, Now wait like a good citizen!!")
   else:
-    m = await mikey.reply(text, buttons = keybo)
+    m = mikey.reply(text, reply_markup = InlineKeyboardMarkup([keybo]))
   
-@draken.on(events.NewMessage(incoming=True, pattern=r'^/start|/start@DRAKENROBOT')) 
-async def start(mikey):
-  if mikey.is_private:
-    await mikey.message.reply(f"Im {bot_name} a bot, i was made to handle requsest!")
-    await draken.send_message(-1001569337079, f"#START\n[{mikey.sender.first_name}](tg://user?id={mikey.sender_id}) started the bot!")
+def start(update: Update, context: CallbackContext):
+  chat = update.effective_chat
+  msg = update.effective_message
+  bot = context.bot
+  if chat.type == "private":
+    msg.reply_text(f"Im {bot_name} a bot made by @DontKnowWhoRU, i was made to handle requsest!")
+    bot.send_message(-1001569337079, f"#START\n[{message.from_user.first_name}](tg://user?id={message.from_user.message_id}) started the bot!")
   else:
-    await mikey.reply("Im up and working!")
+    msg.reply_text("Im up and working!")
 
-@draken.on(events.CallbackQuery(pattern=b'recomp'))
-async def de(mikey):
-  await mikey.delete()
+def de(update: Update, context: CallbackContext):
+  query = update.callback_query 
+  query.message.delete()
 
 print('Im online!!!')
 
 takemichi.start()
-draken.start()
+draken.start_polling()
 draken.run_until_disconnected()
 takemichi.run_until_disconnected()
