c09889552cfe89da08f58c4335ee0f26972632ee
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-07-11 12:56:03 +0530

    update torrentscrape.py

--- torrentscrape.py
+++ torrentscrape.py
@@ -3,9 +3,9 @@ from py1337x import py1337x
 torrents= py1337x(proxy='1337xx.to')
 
 class thirteenX:
-  def __init__(self, query, link):
+  def __init__(self, query, torrentId):
     self.query = query
-    self.link = link
+    self.torrentId = torrentId
     
   def search(query):
     search = torrents.search(query)
@@ -19,7 +19,7 @@ class thirteenX:
       result.append(res)
     return result
       
-  def get_info(link):
-    info = torrents.info(link=link)
+  def get_info(torrentId):
+    info = torrents.info(torrentId=torrentId)
     return [info.get('name'), info.get('category'), info.get('leechers'), info.get('seeders'), info.get('magnetLink'), info.get('size')]
     
